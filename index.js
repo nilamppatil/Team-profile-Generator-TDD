1010fb7f5c5a0d96deb47eb8324109dfba84e625
nilamppatil <nilamppatil.89@gmail.com>
2023-03-18 22:57:14 +0000

    solution for team generator'

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const appMenu = () =>{
   function buildTeam(){
       if (!fs.existsSync(OUTPUT_DIR))
       {
-          fs.mkdirSync(OUTPUT_DIR)
+          fs.mkdirSync(OUTPUT_DIR);
       }
    fs.writeFileSync(outputPath,render(teamMembers),'utf-8');
   }
@@ -178,4 +178,3 @@ const appMenu = () =>{
      createManager();
 };
 appMenu();
-
